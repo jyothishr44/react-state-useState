79ba99f314f328d8b27aebbff868e61acf8b43c4
jyothish <jyothishjyo645@gmail.com>
2025-10-29 12:13:19 +0530

    Add Fruit & Fruits components, App.jsx, main.jsx and index.html for list rendering demo

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,10 @@
-import React from "react";
-import Person from "./components/Person";
-import Welcome from "./components/Welcome";
+import Fruits from "./components/Fruits";
 
 function App() {
-  const person = { name: "Jyothish", age: 21, gender: "Male" };
   return (
-    <>
-      <Welcome />
-      <h1>Displaying Components</h1>
-      <Person person={person} />
-      <Person person={{ name: "Mai", age: 20, gender: "Female" }} />
-    </>
+    <div>
+      <Fruits />
+    </div>
   );
 }
 
